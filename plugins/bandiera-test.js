8d8da9dcfa46e08d7606ca681dd6cb411a742eca
Bonzino <falconemarco511@gmail.com>
2026-03-18 08:21:49 +0100

    Update bandiera-test.js

--- plugins/bandiera-test.js
+++ plugins/bandiera-test.js
@@ -564,7 +564,7 @@ let handler = async (m, { conn, command, isAdmin }) => {
 handler.before = bandieraBefore
 handler.help = ['bandiera', 'skipbandiera']
 handler.tags = ['giochi']
-handler.command = ['test']
+handler.command = ['bandiera','skipbandiera',]
 handler.group = true
 
 export default handler
